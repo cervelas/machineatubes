4ee93ba0b7611d6f5412ec3a2d3a8ea75c9d11b1
clem <493151+cervelas@users.noreply.github.com>
2023-07-23 11:03:40 +0200

    first commit

---
+++ machineatubes/parser.py
@@ -0,0 +1,140 @@
+
+import xml.etree.ElementTree as ET
+import json
+
+from machineatubes.tube import Tube, MidiNote, VideoNote
+
+def parseJSON2Score(jsonfile, verbose=False):
+    '''
+    Parse XML file to SCore structure
+    '''
+
+    struct = {}
+
+    with open(jsonfile, 'r') as f:
+        struct = json.load(f)
+
+    score = Tube()
+    
+    score.bpm = int(struct.get("tempo", 120))
+
+    score.beat_time = 4
+    score.beat_type = 4
+
+    max_length = 0
+
+    for name, part in struct.get("song").items():
+        score.parts[name] = { 
+            "name": name,
+            "type": part.get("type"),
+            "channel": part.get("channel", part["type"]),
+        }
+
+        if part["type"] == "notes":
+            midi_channel = int(part["channel"])
+            if verbose:
+                print("\n\nPART %s" % name)
+            
+            if len(part.get("notes", [])) == 0:
+                print("Empty Part !!")
+            
+            notes = part.get("notes", [])
+
+            for note in notes:
+                        
+                duration = int(note.get("duration", score.beat_time))
+                
+                b = int(note["beat"])
+
+                if b > max_length:
+                    max_length = b
+
+                score.midinote( b, 
+                            MidiNote(int(note["note"]),
+                                duration=duration,
+                                channel=midi_channel)
+                            )
+
+        elif part["type"] == "video":
+            
+            notes = part.get("triggers", [])
+
+            for note in notes:
+                
+                b = int(note["beat"])
+
+                if b > max_length:
+                    max_length = b
+
+                score.videonote( b, 
+                            VideoNote(note["file"],
+                                position=note["position"])
+                            )
+    score.measures = int(max_length / score.beat_type) + 1
+
+    return score
+
+
+def parseMXML2Score(mxmlfile, verbose=False):
+    '''
+    Parse XML file to SCore structure
+    '''
+    tree = ET.parse(mxmlfile)
+    root = tree.getroot()
+
+    score = Tube()
+
+    for part in root.find('./part-list').findall('score-part'):
+        score.parts[part.attrib["id"]] = { 
+            "name": part.find('./part-name').text,
+            "type": "notes",
+            "channel": int(part.find('./midi-instrument/midi-channel').text),
+        }
+    
+    for part in root.findall('./part'):
+        midi_channel = score.parts[part.attrib["id"]]["channel"]
+        if verbose:
+            print("\n\nPART %s" % score.parts[part.attrib["id"]]["name"])
+        number = 0
+        for measure in part.findall('./measure'):
+            snd = measure.findall('./sound')
+            if len(snd) > 0:
+                score.bpm = int(snd[0].attrib['tempo'])
+
+            if measure.find('./attributes/time'):
+                score.beat_time = int(measure.find('./attributes/time/beats').text)
+                score.beat_type = int(measure.find('./attributes/time/beat-type').text)
+
+            division = measure.find('./attributes/divisions')
+            if division is not None:
+                division = 1 / int(measure.find('./attributes/divisions').text)
+            else:
+                division = 1
+            
+            number = int(measure.attrib["number"])
+
+
+            offset = number*score.beat_time
+            for note in measure.findall('./note'):
+                alt = note.find('./pitch/alter')
+                if alt is not None and int(alt.text) == 1:
+                    alt = "#"
+                    
+                duration = score.beat_time
+                if note.find('./duration') is not None:
+                    duration = int(note.find('./duration').text)
+                
+                if note.find('./pitch') is not None: 
+                    n = MidiNote("".join([ note.find('./pitch/step').text, 
+                                        alt or "",
+                                        note.find('./pitch/octave').text ]
+                                    ),
+                                    duration=duration,
+                                    channel=midi_channel)
+                    score.midinote( offset, n )
+                offset += duration
+        
+        if score.measures < number:
+            score.measures = number
+
+    return score
---
+++ machineatubes/tube.py
@@ -0,0 +1,163 @@
+import time
+import json
+import threading
+
+import rtmidi2 as rm
+
+abort = threading.Event()
+
+out = rm.MidiOut() # we may need more than one
+
+notes2midi = { rm.midi2note(i): i for i in range(-255, 255) }
+
+t = time.perf_counter()
+
+def initsleep():
+    global t
+    t = time.perf_counter()
+
+def nanosleep(s):
+    ''' high precision sleep timer, self-correct with drift detection '''
+    global t
+    first = True
+    while time.perf_counter() - t < s:
+        first = False
+        continue
+    if first:
+        print("WARNING: timer drifted %ss" % s)
+    t2 = time.perf_counter()
+    t = t2
+
+class Tube():
+
+    def __init__(self) -> None:
+        self.name = ""
+        self.notes = {}
+        self.beat_time = 4
+        self.beat_type = 4
+        self.measures = 0
+        self.bpm = None
+        self.beat_duration = 0
+        self.parts = {}
+        self.start_time = 0
+        self.time_signature = 0
+        self.divisions = 16
+
+    def duration(self):
+        '''
+        Duration in seconds
+        '''
+        return 60 * ( ( self.measures * self.beat_type ) / ( self.bpm ) )
+
+    def progress(self, b):
+        m = int(b / self.measures)
+        mcount = "%s/%s " % (m, self.measures)
+        t = " %0.2f" % (time.perf_counter() - self.start_time)
+        return mcount + '#' * m + ('o' * (self.measures - m)) + t
+
+    def midinote(self, beat, note):
+        #if args.verbose:
+        #    print("\tbeat %s added %s duration %s" % (beat, note.hnote, note.duration))
+        note.offset = beat
+        self.__update(beat + note.duration)
+        notes = self.notes.get(beat, [])
+        self.notes.update( { beat: notes + [ note ] } )
+        notes = self.notes.get(beat+note.duration, [])
+        self.notes.update( { beat+note.duration: notes + [ note ] } )
+
+    def videonote(self, beat, note):
+        note.beat = beat
+        notes = self.notes.get(beat, [])
+        self.notes.update( { beat: notes + [ note ] } )
+
+    def __update(self, offset):
+        for i in range(offset):
+            self.notes.update( { i: self.notes.get(i, []) } )
+
+    def export(self, file):
+        json_dict = {   "name": self.name, 
+                        "tempo":  self.bpm,
+                        "song": {} }
+        for name, part in self.parts.items():
+            if part["type"] == "notes":
+                json_dict["song"][name] = { 
+                        "type": part["type"],
+                        "channel": part["channel"],
+                        "notes": []
+                    }
+        
+                for beat, notes in self.notes.items():
+                    for note in notes:
+                        if note.channel == part["channel"] and beat == note.offset:
+                            json_dict["song"][name]["notes"].append(
+                                { "beat": beat, "note": note.note, "duration": note.duration }
+                            )
+                
+        with open(file, 'w') as f:
+            json.dump(json_dict, f, indent=4)
+
+    def play(self, window=False, verbose=False):
+        initsleep()
+        self.start_time = time.perf_counter()
+        for b, notes in self.notes.items():
+            if abort.is_set():
+                print("aborting...")
+                break
+            for note in notes:
+                note.play(b, verbose)
+            if verbose:
+                if len(notes) == 0:
+                    print(b)
+            else:
+                print( self.progress(b), end='\r' )
+            nanosleep( ( 60 / self.bpm ) )
+        print("")
+        print("END")
+
+class Note():
+    def play(self, i, verbose=False):
+        raise NotImplementedError()
+
+class MidiNote():
+    def __init__(self, note, duration = 4, velo=127, offset=0, channel=1) -> None:
+        self.note = note
+        self.offset = offset
+        self.duration = duration
+        self.velo = velo
+        self.channel = channel
+        
+        # easy notation support
+        if isinstance(note, str):
+            self.note = notes2midi[note]
+
+        self.hnote = rm.midi2note(self.note)
+
+        if duration < 1:
+            self.duration = int(duration * 16)
+        if offset < 1:
+            self.offset = int(offset * 16)
+
+    def play(self, i, verbose=False):
+        if i == self.offset:
+            out.send_noteon(self.channel-1, self.note, self.velo)
+            if verbose:
+                print("%s note on %s channel %s duration %s" % ( i, self.note, self.channel, self.duration) )
+        elif i == self.offset + self.duration:
+            out.send_noteoff(self.channel-1, self.note)
+            if verbose:
+                print("%s note off %s channel %s" % ( i, self.note, self.channel) )
+
+class VideoNote():
+
+    window = False
+
+    def __init__(self, file, position) -> None:
+        self.file = file
+        self.beat = 0
+        self.position = position
+
+    def play(self, i, verbose=False):
+        if i == self.beat:
+            print("play %s" % self.file)
+            VideoNote.window.evaluate_js("playvid('%s','%s')" % (self.file, self.position))
+            
