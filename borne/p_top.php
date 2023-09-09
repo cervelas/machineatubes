36021bc4c156c492c7c2d3304f101f270eff0a57
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-09 16:32:14 +0200

    Update p_top.php

--- borne/p_top.php
+++ borne/p_top.php
@@ -13,31 +13,21 @@ $keys_used = array();
 
 $note_pitch = $tubet->getToplinePitch();
 
+
 foreach($format as $key=>$section) {
     $section_length = $general_lengths[$section];
+
     if (in_array($section, $go_topline)) {
-        $keys_used []= $key;
-        for($x=0; $x<$section_length; $x++) {
-            $json .= '{
+
+        $json .= '{
                 "beat": ' . ($measure * 4) . ',
                 "note": ' . $note_pitch . ',
                 "duration": 4
-                 }';
-
-            if($x<$section_length-1){
-                $json .= ',';
-            }
-
-            $measure++;
-        }
-
-    }else{
-        $measure += $general_lengths[$section];
+        },';
     }
 
-    if($key != array_key_last($format) && in_array($key,$keys_used)){
-        $json .= ',';
-    }
+    $measure += $general_lengths[$section];
+
 }
 
 $json = rtrim($json, ',');
