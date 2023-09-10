9ed1905854fe02f4543bf45af96db13d39a68862
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-10 10:38:29 +0200

    Update p6.php

--- borne/p6.php
+++ borne/p6.php
@@ -9,7 +9,9 @@ $json .= '"drums": {
                 ';
 
 $go_drums_note = $tube6->getGoDrums($song_style);
-$go_addons_note = $tube6->getAddons($song_addons);
+$go_clap_note = $tube6->getAddons('clap');
+$go_hh_note = $tube6->getAddons('hh');
+$go_kick_note = $tube6->getAddons('kick');
 $measure = 1;
 
 foreach($format as $key=>$section) {
@@ -26,8 +28,26 @@ foreach($format as $key=>$section) {
         },';
     }
 
-    if (in_array($section, $go_addons)) {
-        $note_pitch_a = $tube6->getChordPitch($go_addons_note['step'], $go_addons_note['do_alter'], $go_addons_note['octave']);
+    if (in_array($section, $go_clap)) {
+        $note_pitch_a = $tube6->getChordPitch($go_clap_note['step'], $go_clap_note['do_alter'], $go_clap_note['octave']);
+        $json .= '{
+                "beat": ' . ($measure * 4) . ',
+                "note": ' . $note_pitch_a . ',
+                "duration": 4
+        },';
+    }
+
+    if (in_array($section, $go_hh)) {
+        $note_pitch_a = $tube6->getChordPitch($go_hh_note['step'], $go_hh_note['do_alter'], $go_hh_note['octave']);
+        $json .= '{
+                "beat": ' . ($measure * 4) . ',
+                "note": ' . $note_pitch_a . ',
+                "duration": 4
+        },';
+    }
+
+    if (in_array($section, $go_kick)) {
+        $note_pitch_a = $tube6->getChordPitch($go_kick_note['step'], $go_kick_note['do_alter'], $go_kick_note['octave']);
         $json .= '{
                 "beat": ' . ($measure * 4) . ',
                 "note": ' . $note_pitch_a . ',
