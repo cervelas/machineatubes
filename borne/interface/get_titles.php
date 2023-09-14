ffbf5d8405af1fdf6140aca17e5f29869bfaf141
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-14 18:24:51 +0200

    Update get_titles.php

--- borne/interface/get_titles.php
+++ borne/interface/get_titles.php
@@ -6,12 +6,13 @@ if(isset($_SESSION['song_mood'])){
     $machine = new Tube();
 
     $_SESSION['song_mood_name'] = $machine->getMoodName($_SESSION['song_mood']);
-    $_SESSION['midi_channel'] = $machine->getChannel($_SESSION['song_mood']);
+    $_SESSION['midi_channel'] = $machine->getChordChannel($_SESSION['song_mood']);
 
-    $song_titles = $machine->getRandomTitles($_SESSION['song_mood'], 4, '../lyrics/');
+    $song_title_ids = $machine->getRandomTitles($_SESSION['song_mood'], 4);
 
-    foreach($song_titles as $song_title){
-        echo '<a class="title_init" href="javascript:submitInfo(\'song_title\',\''.urlencode(addslashes($song_title)).'\');">'.$song_title.'</a>';
+    foreach($song_title_ids as $song_title_id){
+        $song_keyword = $machine->getSongInfo($song_title_id)['keyword'];
+        echo '<a class="title_init" href="javascript:submitInfo(\'song_title_id\',\''.$song_title_id.'\');">'.$song_keyword.'</a>';
     }
 }
 
