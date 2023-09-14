0b559fdff46da847d965244c81ed2b3b1cfb86ae
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-14 09:37:02 +0200

    Update json_init.php

--- borne/json_init.php
+++ borne/json_init.php
@@ -4,7 +4,7 @@ $json = '{
     "name":"'.stripslashes($_SESSION['song_title']).'",
     "tempo": '.$_SESSION['song_tempo'].',
     "ambiance": "'.$_SESSION['song_mood_name'].'",
-    "style": "'.$_SESSION['song_style'].'",
+    "style": "'.$_SESSION['song_style'].'_'.$_SESSION['variant'].'",
     "prenom": "'.stripslashes($_SESSION['user_name']).'",
     "numero": '.$_SESSION['song_id'].',
     "song": {';
@@ -15,13 +15,36 @@ include_once('p5.php');
 include_once('p6.php');
 include_once('p8.php');
 include_once('p_top.php');
-
 include_once('lyrics.php');
 
 $json .= '}
 }';
 
-
+echo '
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+    <title>La Machine à Tubes</title>
+</head>
+<body>
+<div id="last_screen" class="bluebg">
+<h2 class="section_title">MY NAME IS FUZZY</h2>
+    <img src="imgs/lamachine.png" />
+    <p>Cr&eacute;ation en cours !<br />
+    Ta chanson porte le num&eacute;ro</p>
+    <p class="big_number">#'.$song_id.'</p>
+    <p>Vous pouvez à présent vous diriger vers la machine.</p>
+   
+    <script>
+    setTimeout(isFinished, 10000); 
+    function isFinished(){
+        window.location = "/new_fuzz/interface/init.php";
+    }
+</script>
+</div>
+</body>
+</html>';
 
 
 ?>
+
