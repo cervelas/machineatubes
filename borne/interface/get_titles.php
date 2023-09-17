6be708e80466af419c61b2800ed77de744c4ec63
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-17 21:29:14 +0200

    Update get_titles.php

--- borne/interface/get_titles.php
+++ borne/interface/get_titles.php
@@ -9,10 +9,12 @@ if(isset($_SESSION['song_mood'])){
     $_SESSION['midi_channel'] = $machine->getChordChannel($_SESSION['song_mood']);
 
     $song_title_ids = $machine->getRandomTitles($_SESSION['song_mood'], 4);
+    $circle_colors = array("green", "yellow", "blue", "red");
 
-    foreach($song_title_ids as $song_title_id){
+    foreach($song_title_ids as $key=>$song_title_id){
+        $circle_class = $circle_colors[$key]."_circle";
         $song_keyword = $machine->getSongInfo($song_title_id)['keyword'];
-        echo '<a class="title_init" href="javascript:submitInfo(\'song_title_id\',\''.$song_title_id.'\');">'.$song_keyword.'</a>';
+        echo '<a class="title_init '.$circle_class.'" href="javascript:submitInfo(\'song_title_id\',\''.$song_title_id.'\');"><span class="bluebg">>'.$song_keyword.'</span></a>';
     }
 }
 
