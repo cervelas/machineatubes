7f0401099f6efd0577dc84e7fa3e196802ee4a2f
7AM <22347432+ttt-amm@users.noreply.github.com>
2023-09-18 21:40:06 +0200

    Update Machine.php

--- borne/Classes/Machine.php
+++ borne/Classes/Machine.php
@@ -16,15 +16,49 @@ class Machine extends Database {
         return $this->query($sql);
     }
 
+    public function insertUsedTitle($title_id){
+        $sql = "INSERT INTO songs_titles_used(song_title_id, used) VALUES($title_id, 1)";
+        return $this->query($sql);
+    }
+
     public function getSongTitles($tonality): array
     {
         $sql = "SELECT * FROM song_titles WHERE mood='$tonality'";
         return $this->query($sql);
     }
 
+    public function getUsedSongTitles(){
+        $sql = "SELECT DISTINCT song_title_id FROM songs_titles_used WHERE used=1";
+        return $this->querySimpleArray($sql,'song_title_id');
+    }
+
+    public function getUnusedSongTitles($tonality){
+        $used_ids = $this->getUsedSongTitles();
+        $sql = "SELECT * FROM song_titles WHERE mood='$tonality'";
+        if(sizeof($used_ids)>0){
+            $used_str = implode(',',$used_ids);
+            $sql .= " AND id NOT IN ($used_str)";
+        }
+        $res = $this->query($sql);
+        if(sizeof($res)>4){
+            return $res;
+        }else{
+            if($this->resetUsedSongTitles($tonality)){
+                return $this->getSongTitles($tonality);
+            }
+        }
+    }
+
+    public function resetUsedSongTitles($tonality){
+        $used_ids = array_column($this->getSongTitles($tonality),'id');
+        $used_str = implode(',',$used_ids);
+        $sql = "UPDATE songs_titles_used SET used=0 WHERE song_title_id IN ($used_str)";
+        return $this->query($sql);
+    }
+
     public function getRandomTitles($tonality, $qty): array
     {
-        $titles = $this->getSongTitles($tonality);
+        $titles = $this->getUnusedSongTitles($tonality);
         $random_keys = array_rand($titles,$qty);
         foreach($random_keys as $key){
             $random[] = $titles[$key]['id'];
