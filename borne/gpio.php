9f5fc7e0ec45cf07b9d82c84afd6d98fe26e5224
Bastien Bron <lamachine@Mac-mini-de-Bastien.local>
2023-09-19 14:54:39 +0200

    gpio stuff

--- borne/gpio.php
+++ borne/gpio.php
@@ -4,18 +4,25 @@ require __DIR__ . '/vendor/autoload.php';
 use PiPHP\GPIO\GPIO;
 use PiPHP\GPIO\Pin\PinInterface;
 
-// Create a GPIO object
-$gpio = new GPIO();
-
-// Retrieve pin 17 and configure it as an output pin
-$pin = $gpio->getOutputPin(17);
 
 function ledsON(){
+
+    // Create a GPIO object
+    $gpio = new GPIO();
+
+    // Retrieve pin 17 and configure it as an output pin
+    $pin = $gpio->getOutputPin(17);
     // Set the value of the pin high (turn it on)
     $pin->setValue(PinInterface::VALUE_HIGH);
 }
 
 function ledsOFF(){
+
+    // Create a GPIO object
+    $gpio = new GPIO();
+
+    // Retrieve pin 17 and configure it as an output pin
+    $pin = $gpio->getOutputPin(17);
     // Set the value of the pin high (turn it on)
     $pin->setValue(PinInterface::VALUE_LOW);
 }
